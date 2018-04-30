4da0583aa4d39ee730a1ad7f804fc30254086cf1
JuicyWeeb <36629563+JuicyWeeb@users.noreply.github.com>
2018-04-30 22:41:44 +0200

    First Commit
    Addes Unity Project to GitHub

---
+++ Sukiyaki/Assets/Scripts/Player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float maxSpeed = 3f;
+    public float speed = 50f;
+    public float jumpPower = 20f;
+
+    public bool grounded;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        anim.SetBool("Grounded",grounded);
+        anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+
+        if(Input.GetAxis("Horizontal") < -0.1f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (Input.GetButtonDown("Jump") && grounded == true)
+        {
+            rb2d.AddForce(Vector2.up * jumpPower);
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+
+        Vector3 easeVelocity = rb2d.velocity;
+        easeVelocity.y = rb2d.velocity.y;
+        easeVelocity.z = 0.05f;
+        easeVelocity.x *= 0.75f;
+
+        //Rör spelaren
+        float h = Input.GetAxis("Horizontal");
+
+        //Fake friktion / lättar på x hastigheten för spelaren.
+        if (grounded == true)
+        {
+            rb2d.velocity = easeVelocity;
+        }
+
+        rb2d.AddForce((Vector2.right * speed) * h);
+        //Begränsar hastigheten för spelaren
+        if(rb2d.velocity.x > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+        }
+        if (rb2d.velocity.x < -maxSpeed)
+        {
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+        }
+
+
+    }
+
+
+}
